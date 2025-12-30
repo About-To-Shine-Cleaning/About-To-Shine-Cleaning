f49a9ac050cc8da17e3fc7b9b0efcd24eca81e15
About-To-Shine-Cleaning <abouttoshinecleaning.atsc@gmail.com>
2025-12-30 15:04:24 -0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
   const toggle = document.querySelector(".nav-toggle");
   const nav = document.querySelector(".main-nav");
 
